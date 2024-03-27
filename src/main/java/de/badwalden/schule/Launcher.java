02111b1fb21efaf665f4a671ddf882912a71dbff
TimeLegend35 <leanfuerst@googlemail.com>
2024-03-27 20:09:31 +0100

    initial commit

---
+++ src/main/java/de/badwalden/schule/Launcher.java
@@ -0,0 +1,7 @@
+package de.badwalden.schule;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
---
+++ src/main/java/de/badwalden/schule/Main.java
@@ -0,0 +1,15 @@
+package de.badwalden.schule;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
